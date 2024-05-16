2cf49f3f3a9f91a1b9f32ff1d7deb04912b897f2
Ali Rahmani <alirahmani.prog@gmail.com>
2024-05-16 11:15:40 +0330

    video manipulation cleanup

--- video-manipulation.py
+++ video-manipulation.py
@@ -6,13 +6,18 @@ video = cv2.VideoCapture("assets/video.avi")
 while True:
     ret, frame = video.read()
 
-    if ret:
-        text = str(datetime.datetime.now())
+    if ret == False:
+        break
 
-        frame = cv2.putText(
-            frame, text, (10, 20), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, (255, 255, 255), 1)
+    text = str(datetime.datetime.now())
 
-        cv2.imshow("video", frame)
+    frame = cv2.putText(
+        frame, text, (10, 20), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, (255, 255, 255), 1)
 
-        if cv2.waitKey(1000) == ord('q'):
-            break
+    cv2.imshow("video", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
